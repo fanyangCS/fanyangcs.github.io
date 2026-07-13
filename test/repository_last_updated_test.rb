52199897dc1568fec6e6f5e2022a4a5f2dff7e74
Ubuntu <azureuser@MyClawFree.qxmr4urm0qxu1cbsd1s3owgxrc.lx.internal.cloudapp.net>
2026-07-13 08:26:51 +0000

    Automate homepage footer update date

---
+++ test/repository_last_updated_test.rb
@@ -0,0 +1,50 @@
+# AI-generated regression coverage for the build-time repository update date.
+require 'fileutils'
+require 'minitest/autorun'
+require 'tmpdir'
+
+module Jekyll
+  class Generator
+    def self.priority(_priority); end
+  end
+end
+
+require_relative '../_plugins/repository-last-updated'
+
+class RepositoryLastUpdatedGeneratorTest < Minitest::Test
+  Site = Struct.new(:source, :config)
+
+  def setup
+    @tmpdir = Dir.mktmpdir('repository-last-updated-test')
+    git('init', '--quiet')
+    git('config', 'user.name', 'Test User')
+    git('config', 'user.email', 'test@example.com')
+    File.write(File.join(@tmpdir, 'index.md'), 'test')
+    git('add', 'index.md')
+    git(
+      'commit', '--quiet', '-m', 'Test commit',
+      env: {
+        'GIT_AUTHOR_DATE' => '2026-07-12T09:10:11Z',
+        'GIT_COMMITTER_DATE' => '2026-07-13T12:35:52+08:00'
+      }
+    )
+  end
+
+  def teardown
+    FileUtils.remove_entry(@tmpdir)
+  end
+
+  def test_exposes_head_committer_timestamp_to_liquid
+    site = Site.new(@tmpdir, {})
+
+    Jekyll::RepositoryLastUpdatedGenerator.new.generate(site)
+
+    assert_equal '2026-07-13T12:35:52+08:00', site.config['repository_last_updated_at']
+  end
+
+  private
+
+  def git(*arguments, env: {})
+    system(env, 'git', *arguments, chdir: @tmpdir, exception: true)
+  end
+end
